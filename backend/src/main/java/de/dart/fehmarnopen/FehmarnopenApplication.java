cc91aca6ea6cb84eb2b6613871206dff26cb0dc5
Nick Schmahl <schmahlnick@gmail.com>
2026-05-23 16:42:57 +0200

    #2 backend setup

---
+++ backend/src/main/java/de/dart/fehmarnopen/FehmarnopenApplication.java
@@ -0,0 +1,13 @@
+package de.dart.fehmarnopen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FehmarnopenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FehmarnopenApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/dart/fehmarnopen/FehmarnopenApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dart.fehmarnopen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FehmarnopenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
